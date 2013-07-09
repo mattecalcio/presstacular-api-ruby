0095b055e768840018231522b606d79bc25d76bd
mattecalcio <mattecalcio@gmail.com>
2013-07-09 13:12:00 -0400

    Update add_subscriber params

--- lib/mailermailer-api-ruby/client.rb
+++ lib/mailermailer-api-ruby/client.rb
@@ -19,16 +19,18 @@ module MMAPI
 
     # Add the specifed subscriber
     #
-    def add_subscriber(subscriber)
+    def add_subscriber(subscriber, send_invite = true, send_welcome = false)
       params = Hash.new
       params["subscriber"] = subscriber
+      params["send_invite"] = send_invite
+      params["send_welcome"] = send_welcome 
       response = @mmapi_rpc_call.executeMethod('addSubscriber', params)
       return response
     end
 
     # Unsubscribes the subscriber associated with the given email
     #
-    def unsub_subscriber(subscriber_email, permanent=false)
+    def unsub_subscriber(subscriber_email, permanent = false)
       params = Hash.new
       params["subscriber_email"] = subscriber_email
       params["permanent"] = permanent
